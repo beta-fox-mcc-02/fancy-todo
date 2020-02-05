5cf0a6adc90138a872b0f3438fa1dd9c3fb864a4
nafies1 <ghuroba.muslim@gmail.com>
2020-02-05 18:42:07 +0700

    add login client and register

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,31 +1,91 @@
 $(document).ready(function(){
-  
-  if (localStorage.getItem('roken')){
-    $("#login").hide()
-    $("#todo").show()
-  } else {
-    $("#login").show()
-    $("#todo").hide()
-  }
-  $("#register").hide()
-  
+  checkLogin()
+  pageNavigator()
 
-  $("#btnHome").click(function(){
-    $("#login").hide()
-    $("#register").hide()
-    $("#todo").show()
+  $("#login").submit(function(e){
+    e.preventDefault()
+    const email = $('#emailLogin').val()
+    const password = $('#passwordLogin').val()
+    $.ajax({
+      url: "http://localhost:3000/auth/login",
+      method: "POST",
+      data: { email, password },
+    })
+      .done(function(user) {
+        console.log(user)
+        localStorage.token = user.token
+        checkLogin()
+      })
+      .fail(function(err) {
+        console.log(err.responseJSON)
+      })
   })
 
+  $("#register").submit(function(e){
+    e.preventDefault()
+    const name = $('#nameRegister').val()
+    const email = $('#emailRegister').val()
+    const password = $('#passwordRegister').val()
+    $.ajax({
+      url: "http://localhost:3000/auth/register",
+      method: "POST",
+      data: { name, email, password },
+    })
+      .done(function(user) {
+        console.log(user)
+        // localStorage.token = user.token
+        // checkLogin()
+      })
+      .fail(function(err) {
+        console.log(err.responseJSON)
+      })
+  })
+
+})
+
+function checkLogin () {
+  if (localStorage.getItem('token')){
+    pageHome()
+    console.log('sudah login')
+  } else {
+    pageLogin()
+    console.log('belum login')
+  }
+  console.log('checked')
+}
+
+function pageNavigator () {
+  // Navigator
+  $("#btnHome").click(function(){
+    pageHome()
+  })
+  
   $("#btnLogin").click(function(){
-    $("#login").show()
-    $("#register").hide()
-    $("#todo").hide()
+    pageLogin()
   })
 
   $("#btnRegister").click(function(){
-    $("#login").hide()
-    $("#register").show()
-    $("#todo").hide()
+    pageRegister()
   })
-})
+  // end of navigator
+}
 
+function pageHome () {
+  $("#loginPage").hide()
+  $("#registerPage").hide()
+  $("#todo").show()
+  $("#navbar").show()
+}
+
+function pageLogin () {
+  $("#loginPage").show()
+  $("#registerPage").hide()
+  $("#navbar").hide()
+}
+
+function pageRegister () {
+  $("#loginPage").hide()
+  $("#registerPage").show()
+  $("#todo").hide()
+  $("#navbar").hide()
+}
