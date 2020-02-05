a5f1e302e3f925b0a7882efe887be6d212179a07
hendry <hendry_wu1984@yahoo.com>
2020-02-06 00:02:08 +0700

    navbar visible done

--- client/js/main.js
+++ client/js/main.js
@@ -11,8 +11,8 @@ function menu(opt){
 
 // visible menu transaction (CRUD) settings
 function menuTrans(opt){
-    opt ? ( $("#menu-list").show(), $("#menu-create").show() )
-    : ( $("#menu-list").hide(), $("#menu-create").hide() );
+    opt ? ( $("#menu-list-todos").show(), $("#menu-create-todos").show() )
+    : ( $("#menu-list-todos").hide(), $("#menu-create-todos").hide() );
 }
 
 // visible container menu settings
@@ -29,9 +29,9 @@ function cont(opt){
 // visible container transaction (CRUD) settings
 function contTrans(opt){
     opt ? (
-        $("#cont-list").show(), $("#cont-create").show()
+        $("#cont-list-todos").show(), $("#cont-create-todos").show()
     ) : (
-        $("#cont-list").hide(), $("#cont-create").hide()
+        $("#cont-list-todos").hide(), $("#cont-create-todos").hide()
     );
 }
 
@@ -82,36 +82,52 @@ function addTodos(token, data){
 
 $(document).ready(function(){
     startMenu();
+console.log(1, localStorage.getItem("token"));
+    const Token = localStorage.getItem("token");
+    // if(localStorage.getItem("token")) {
+    //     // $("#menu-logout").show();
+    //     // $("#menu-register").hide();
+    //     // $("#menu-login").hide();
 
-    if(localStorage.getItem("token")) {
-        // $("#menu-logout").show();
-        // $("#menu-register").hide();
-        // $("#menu-login").hide();
-
-        // getTodos(localStorage.getItem("token"));
-    }
+    //     // getTodos(localStorage.getItem("token"));
+    // }
 
     $("#menu-home").click(function () {
         // $("#cont-login").hide();
         // $("#cont-register").hide();
         // $("#cont-home").show();
+        if(Token) {
+            menu(false);
+            menuTrans(true);
+            cont(false);
+            contTrans(false);
 
-       
+            $("#menu-logout").show();
+            $("#menu-home").show();
+            $("#cont-home").show();
+        } else startMenu();
     })
 
     $("#menu-login").click(function () {
         menu(true);
+        menuTrans(false);
         cont(false);
         contTrans(false);
-
+        $("#menu-logout").hide();
         $("#cont-login").show();
-        // $("#cont-home").hide();
     })
 
     $("#menu-register").click(function () {
         // $("#cont-login").hide();
         // $("#cont-register").show();
         // $("#cont-home").hide();
+
+        menu(true);
+        menuTrans(false);
+        cont(false);
+        contTrans(false);
+        $("#menu-logout").hide();
+        $("#cont-login").show();
     })
 
     $("#menu-logout").click(function () {
@@ -119,16 +135,17 @@ $(document).ready(function(){
         // $("#menu-register").show();
         // $("#menu-login").show();
         // $("#cont-home").hide();
+        startMenu();
     })
 
-    $("#menu-list-todos").click(function () {
-        menu(false);
-        $("#menu-home").show();
-        $("#menu-logout").show();
+    // $("#menu-list-todos").click(function () {
+    //     menu(false);
+    //     $("#menu-home").show();
+    //     $("#menu-logout").show();
         
-        menuTrans(true);
-        console.log(getTodos(localStorage.getItem("token")));
-    })
+    //     menuTrans(true);
+    //     console.log(getTodos(localStorage.getItem("token")));
+    // })
 
     $("#login").click(function(){       
         let email = $("#email").val();
@@ -193,35 +210,35 @@ $(document).ready(function(){
         }
     });
 
-    $("#register").click(function(){       
-        let email = $("#regis-email").val();
-        let password = $("#regis-password").val();
-        let password2 = $("#regis-password2").val();
+    // $("#register").click(function(){       
+    //     let email = $("#regis-email").val();
+    //     let password = $("#regis-password").val();
+    //     let password2 = $("#regis-password2").val();
 
-        if(password !== password2){
-            $('input[type="password"],input[type="password2"]').css("border","2px solid red");
-            $('input[type="password"],input[type="password2"]').css("box-shadow","0 0 3px red");
-            console.log("Password and confirm password not match...!!!!!!");
-        } else if(password === '' || password2 === '') {
-            $('input[type="password"],input[type="password2"]').css("border","2px solid red");
-            $('input[type="password"],input[type="password2"]').css("box-shadow","0 0 3px red");
-            console.log("Password or confirm password don't blank...!!!!!!");
-        } else if(email === '') {
-            $('input[type="email"]').css("border","2px solid red");
-            $('input[type="email"]').css("box-shadow","0 0 3px red");
-            console.log("Email don't blank...!!!!!!");
-        } else {
-            $.ajax({
-                method: "POST",
-                url: "http://localhost:3000/users/create",
-                data: { email, password }
-            })
-                .done(function(data) {
-                    localStorage.setItem("token", data.token)
-                })
-                .fail(function() {
-                    console.log("Aborted create user process");
-                })
-        }
-    });
+    //     if(password !== password2){
+    //         $('input[type="password"],input[type="password2"]').css("border","2px solid red");
+    //         $('input[type="password"],input[type="password2"]').css("box-shadow","0 0 3px red");
+    //         console.log("Password and confirm password not match...!!!!!!");
+    //     } else if(password === '' || password2 === '') {
+    //         $('input[type="password"],input[type="password2"]').css("border","2px solid red");
+    //         $('input[type="password"],input[type="password2"]').css("box-shadow","0 0 3px red");
+    //         console.log("Password or confirm password don't blank...!!!!!!");
+    //     } else if(email === '') {
+    //         $('input[type="email"]').css("border","2px solid red");
+    //         $('input[type="email"]').css("box-shadow","0 0 3px red");
+    //         console.log("Email don't blank...!!!!!!");
+    //     } else {
+    //         $.ajax({
+    //             method: "POST",
+    //             url: "http://localhost:3000/users/create",
+    //             data: { email, password }
+    //         })
+    //             .done(function(data) {
+    //                 localStorage.setItem("token", data.token)
+    //             })
+    //             .fail(function() {
+    //                 console.log("Aborted create user process");
+    //             })
+    //     }
+    // });
 });
