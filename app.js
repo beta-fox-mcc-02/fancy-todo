c401acdd3cc80c5511e2da8f046ea6fdeb7764a2
Ulrich Wake <uulwake@gmail.com>
2021-10-05 15:21:47 +0700

    set env

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-if(process.env.NODE_ENV === 'development') require('dotenv').config();
+if (process.env.NODE_ENV === "development") require("dotenv").config();
 
-const express = require('express'); const app = express();
+process.env.NODE_ENV = "development";
+process.env.SALT = "SALT";
+process.env.SECRET_KEY = "SECRET_KEY";
+process.env.SECRET_PASSWORD = "SECRET_PASSWORD";
+
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
-const indexRouter = require('./routes');
-const errorHandler = require('./middlewares/errorHandler');
-const cors = require('cors');
+const indexRouter = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
 
 // middleware
 app.use(cors());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(indexRouter);
 app.use(errorHandler);
 
-app.listen(port, ()=> console.log(`Listen to port ${port}`));
+app.listen(port, () => console.log(`Listen to port ${port}`));
